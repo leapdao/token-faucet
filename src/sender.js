dea671b01badfd650cb827f734ffa2f37fc8b4d0
Kosta Korenkov <kosta@whoot.me>
2019-04-10 13:09:37 +0300

    chore: use SSM Properties from boilerplate

--- src/sender.js
+++ src/sender.js
@@ -4,24 +4,15 @@
  * This source code is licensed under the Mozilla Public License Version 2.0
  * found in the LICENSE file in the root directory of this source tree.
  */
-const AWS = require('aws-sdk');
+
 const { bufferToHex, privateToAddress, toBuffer } = require('ethereumjs-util');
-
+const { Properties } = require('leap-lambda-boilerplate');
 const dispenseTokens = require('./dispenseTokens');
-
-const ssm = new AWS.SSM();
-
-const readEncryptedProperty = name => new Promise((resolve, reject) => {
-  ssm.getParameter({ Name: name, WithDecryption: true }, (err, data) => {
-    if (err) return reject(err);
-    return resolve(data.Parameter.Value);
-  });
-});
 
 exports.handler = async (event) => {
   const provider = process.env.PROVIDER_URL;
   const amount = process.env.AMOUNT;
-  const privKey = await readEncryptedProperty(`/faucet/${process.env.ENV}/PRIV_KEY`);
+  const privKey = await Properties.readEncrypted(`/faucet/${process.env.ENV}/PRIV_KEY`);
   const faucetAddr = bufferToHex(privateToAddress(toBuffer(privKey)));
 
   const requests = event.Records.map(r => r.body);
