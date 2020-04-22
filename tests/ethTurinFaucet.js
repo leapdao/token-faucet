334403244d1ff6c274163cc25a8b83b442e0fa51
Keno Budde <Clearwood@users.noreply.github.com>
2020-04-22 20:33:25 +0200

    fixed test

--- tests/ethTurinFaucet.js
+++ tests/ethTurinFaucet.js
@@ -18,13 +18,17 @@ const queue = {
 };
 
 describe("EthTurin Faucet", () => {
-  // describe('checkSignature()', () => {
-  //   it('should check signature', () => {
-  //   	const sig = '0x6019dfb626d5747ed84f49be3fe987206daeb22422145735cb9d863e991775d1227cd676eb69db5ed3ad98bfb32205e2bd2b271ba8806430aa1400fc7d28d2b201';
-  //   	const rsp = checkSignature('0x49A0D89e69402FF1F03168De804BE755C3a57cCc', sig);
-  //   	chai.expect(rsp).to.equal('0x89ab6d3c799d35f5b17194ee7f07253856a67949');
-  //   });
-  // });
+  describe("checkSignature()", () => {
+    it("should check signature", () => {
+      const sig =
+        "0xe828e92dc11e85a42704beb90cc34f6a384933f98b082533ec9d609bae6f569547fd5024c75711ae3a09d7eb41013583e9cc1abc0f50c22f57264a75d46630a901";
+      const rsp = checkSignature(
+        "0xeDfb8EFBB5a040f38d0B9b3E24c7b366C8DED412",
+        sig
+      );
+      chai.expect(rsp).to.equal("0x89AB6D3C799d35f5b17194Ee7F07253856A67949");
+    });
+  });
 
   describe("handleEthTurin()", () => {
     it("should allow happy case", async () => {
