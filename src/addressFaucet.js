44cc8a7b7dddd545ffd7dd5a41dc43f71d20d87d
Keno Budde <Clearwood@users.noreply.github.com>
2020-04-22 20:28:20 +0200

    fixed checking address

--- src/addressFaucet.js
+++ src/addressFaucet.js
@@ -5,30 +5,29 @@
  * found in the LICENSE file in the root directory of this source tree.
  */
 
-const AWS = require('aws-sdk');
-const { isValidAddress } = require('ethereumjs-util');
-const { Queue, Errors } = require('leap-lambda-boilerplate');
-const Db = require('./utils/db');
-const util = require('ethereumjs-util');
-
+const AWS = require("aws-sdk");
+const { isValidAddress } = require("ethereumjs-util");
+const { Queue, Errors } = require("leap-lambda-boilerplate");
+const Db = require("./utils/db");
+const util = require("ethereumjs-util");
+const Web3 = require("web3");
 
 const checkSignature = (nonce, signature) => {
-
-  nonce = "\x19Ethereum Signed Message:\n" + nonce.length + nonce;
-  nonce = util.keccak(nonce);
-  const sig = signature;
-  const {v, r, s} = util.fromRpcSig(sig);
-  const pubKey  = util.ecrecover(util.toBuffer(nonce), v, r, s);
-  const addrBuf = util.pubToAddress(pubKey);
-  const addr    = util.bufferToHex(addrBuf);
-  return addr;
-}
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider(
+      "https://ropsten.infura.io/v3/f039330d8fb747e48a7ce98f51400d65"
+    )
+  );
+  const msg = web3.utils.sha3(nonce);
+  let key = web3.eth.accounts.recover(msg, signature);
+  return key;
+};
 
 exports.checkSignature = checkSignature;
 
 exports.handleEthTurin = async (body, web3, db, queue) => {
   const { created } = await db.getAddr(body.address);
-  const dayAgo = Date.now() - (24 * 60 * 60 * 1000);
+  const dayAgo = Date.now() - 24 * 60 * 60 * 1000;
   if (dayAgo < created) {
     throw new Errors.BadRequest(`not enough time passed since the last claim`);
   }
@@ -40,29 +39,30 @@ exports.handleEthTurin = async (body, web3, db, queue) => {
 
   // todo: check ownership of NFT
 
-  await queue.put(JSON.stringify({address, color: body.color}));
+  await queue.put(JSON.stringify({ address, color: body.color }));
   // todo: also send balance card
   await db.setAddr(address);
 
-  return { 
+  return {
     statusCode: 200,
-    body: { address, color }
+    body: { address, color },
   };
-}
+};
 
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = true;
 
-  const body = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+  const body =
+    typeof event.body === "string" ? JSON.parse(event.body) : event.body;
   const address = body.address;
   const color = parseInt(body.color) || 0;
 
-  const awsAccountId = context.invokedFunctionArn.split(':')[4];
+  const awsAccountId = context.invokedFunctionArn.split(":")[4];
   const queueUrl = `https://sqs.${process.env.REGION}.amazonaws.com/${awsAccountId}/${process.env.QUEUE_NAME}`;
 
   const queue = new Queue(new AWS.SQS(), queueUrl);
   const db = new Db(process.env.TABLE_NAME);
-  
+
   if (!isValidAddress(address)) {
     throw new Errors.BadRequest(`Not a valid Ethereum address: ${address}`);
   }
@@ -72,16 +72,16 @@ exports.handler = async (event, context) => {
   }
 
   const { created } = await db.getAddr(address);
-  const dayAgo = Date.now() - (24 * 60 * 60 * 1000);
+  const dayAgo = Date.now() - 24 * 60 * 60 * 1000;
   if (dayAgo < created) {
     throw new Errors.BadRequest(`not enough time passed since the last claim`);
   }
 
-  await queue.put(JSON.stringify({address, color}));
+  await queue.put(JSON.stringify({ address, color }));
   await db.setAddr(address);
 
-  return { 
+  return {
     statusCode: 200,
-    body: { address, color }
+    body: { address, color },
   };
 };
--- tests/ethTurinFaucet.js
+++ tests/ethTurinFaucet.js
@@ -1,10 +1,10 @@
-const chai = require('chai');
-const sinonChai = require('sinon-chai');
-const sinon = require('sinon');
-const { it, describe, afterEach, beforeEach } = require('mocha');
-const { checkSignature, handleEthTurin } = require('../src/addressFaucet.js');
+const chai = require("chai");
+const sinonChai = require("sinon-chai");
+const sinon = require("sinon");
+const { it, describe, afterEach, beforeEach } = require("mocha");
+const { checkSignature, handleEthTurin } = require("../src/addressFaucet.js");
 
-const Db = require('../src/utils/db');
+const Db = require("../src/utils/db");
 
 chai.use(sinonChai);
 
@@ -17,7 +17,7 @@ const queue = {
   put() {},
 };
 
-describe('EthTurin Faucet', () => {
+describe("EthTurin Faucet", () => {
   // describe('checkSignature()', () => {
   //   it('should check signature', () => {
   //   	const sig = '0x6019dfb626d5747ed84f49be3fe987206daeb22422145735cb9d863e991775d1227cd676eb69db5ed3ad98bfb32205e2bd2b271ba8806430aa1400fc7d28d2b201';
@@ -26,24 +26,27 @@ describe('EthTurin Faucet', () => {
   //   });
   // });
 
-  describe('handleEthTurin()', () => {
-    it('should allow happy case', async () => {
-    	const body = {
-    		address: '0x89ab6d3c799d35f5b17194ee7f07253856a67949',
-    		color: 4,
-    		sig: '0x6019dfb626d5747ed84f49be3fe987206daeb22422145735cb9d863e991775d1227cd676eb69db5ed3ad98bfb32205e2bd2b271ba8806430aa1400fc7d28d2b201',
-    		toAddress: '0x49A0D89e69402FF1F03168De804BE755C3a57cCc'
-    	}
+  describe("handleEthTurin()", () => {
+    it("should allow happy case", async () => {
+      const body = {
+        address: "0x89ab6d3c799d35f5b17194ee7f07253856a67949",
+        color: 4,
+        sig:
+          "0xe828e92dc11e85a42704beb90cc34f6a384933f98b082533ec9d609bae6f569547fd5024c75711ae3a09d7eb41013583e9cc1abc0f50c22f57264a75d46630a901",
+        toAddress: "0xeDfb8EFBB5a040f38d0B9b3E24c7b366C8DED412",
+      };
 
-    	const web3 = null;
+      const web3 = null;
 
-	    sinon.stub(sdb, 'getAddr').resolves({ created: Date.now() - 25 * 60 * 60 * 1000 });
-	    sinon.stub(sdb, 'setAddr').yields(null, {});
+      sinon
+        .stub(sdb, "getAddr")
+        .resolves({ created: Date.now() - 25 * 60 * 60 * 1000 });
+      sinon.stub(sdb, "setAddr").yields(null, {});
 
-	    sinon.stub(queue, 'put').yields(null, {});
+      sinon.stub(queue, "put").yields(null, {});
 
-	    rsp = await handleEthTurin(body, web3, sdb, queue);
-	    console.log(rsp);
+      rsp = await handleEthTurin(body, web3, sdb, queue);
+      console.log(rsp);
     });
   });
 });
